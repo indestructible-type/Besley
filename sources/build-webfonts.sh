f49037f44832ab9ec6113ce821173ef80ea088a1
EwonRael <ewonrael@yahoo.com>
2023-01-09 01:27:35 +0100

    Besley 4.0

--- sources/build-webfonts.sh
+++ sources/build-webfonts.sh
@@ -5,7 +5,14 @@ if [[ ! -d ../fonts/webfonts ]]; then
     mkdir ../fonts/webfonts
 fi
 
-for font in ../fonts/{variable,ttf}/*; do
-    FN=`basename "$font"`
+for font in ../fonts/ttf/*; do
+    FA=`basename "$font"`
+    FN=${FA::-4}
+    fonttools ttLib.woff2 compress "$font" --output-file "../fonts/webfonts/"$FN".woff2"
+done
+
+for font in ../fonts/variable/*; do
+    FA=`basename "$font"`
+    FN=${FA::-4}
     fonttools ttLib.woff2 compress "$font" --output-file "../fonts/webfonts/"$FN".woff2"
 done
